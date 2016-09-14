4c7316cc11915047a46cae6276d062520afc24d5
Vadym <vadimromanenko@yandex.ru>
2016-09-14 20:54:42 +0300

    pre-release

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 (function () {
 'use strict';
 
-angular.module('DIApp', [])
-.controller('DIController', DIController);
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-function DIController ($scope,
-                       $filter,
-                       $injector) {
-  $scope.name = "Yaakov";
+LunchCheckController.$inject=['$scope'];
+function LunchCheckController ($scope) {
+  $scope.editingDishes = "";
+  $scope.userMessage = "";
 
-  $scope.upper = function () {
-    var upCase = $filter('uppercase');
-    $scope.name = upCase($scope.name);
-  };
+  $scope.checkDishes = function () {
+      var dishes = $scope.editingDishes;
+      dishes = normalizeString(dishes);
+      if (dishes === "")
+      {
+        $scope.userMessage = "Please enter non-empty data first";
+        return;
+      }
 
-  console.log($injector.annotate(DIController));
+      var cnt = dishes.split(",").length
+      console.log(cnt);
+      if (cnt < 4)
+      {
+        $scope.userMessage = "Enjoy!";
+      }
+      else {
+        $scope.userMessage = "Too much!";
+      }
+  }
+
+  function normalizeString(str) {
+    if (str === "")
+      return "";
+    var localStr = str.replace(/ /g,'');
+    var pos;
+    //remove trailing commas
+    while(localStr.length > 0 && (localStr[0] === "," || localStr[localStr.length-1] === ","))
+    {
+      if (localStr[0] === ",")
+      {
+        localStr = localStr.substring(1);
+        continue;
+      }
+      if (localStr[localStr.length-1] === ",")
+      {
+        localStr = localStr.substring(0, localStr.length-1);
+        continue;
+      }
+    }
+    console.log(localStr);
+    return localStr;
+  }
 }
-
-function AnnonateMe(name, job, blah) {
-  return "Blah!";
-}
-
-console.log(DIController.toString());
-
 })();
